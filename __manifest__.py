d6cadee8917ee8529e6d5ea7bb3738ef4d9777a9
KevTVKevin <xxkevtvxx@gmail.com>
2021-05-22 14:53:15 +0200

    feat(gigaclub_translation): initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'GigaClub Translation',
+    'version': '14.0.1.0.0',
+    'summary': 'GigaClub Translation Module',
+    'category': 'Translation',
+    'author': 'GigaClub',
+    'website': 'https://GigaClub.net/',
+    'license': 'MIT',
+    'depends': ['gigaclub_base'],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False
+}
