6f77f27a3dd5074bafb29620823ca886bc9774e0
Kevin <kevin@gigaclub.net>
2021-07-11 12:48:04 +0200

    imp(gigaclub_translation): changed one2many to many2many

--- models/gc_translation_entry.py
+++ models/gc_translation_entry.py
@@ -6,4 +6,5 @@ class GCTranslationEntry(models.Model):
 
     content = fields.Text(required=True)
     translation_id = fields.Many2one(comodel_name="gc.translation")
+    translation_ids = fields.Many2many(comodel_name="gc.translation")
     language_id = fields.Many2one(comodel_name="gc.language")
